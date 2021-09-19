d265066a62ccf8e63666519556bbf5d45165400a
ccleary93 <65905077+ccleary93@users.noreply.github.com>
2021-09-19 18:56:34 +0100

    Remove selected_game object
    Object replaced with a hidden field in the form in
    'multiple.html'. The form is now used to pass the 
    game details to the select function.
    
    Reliance on the class object would not have correctly
    scaled to multiple users.

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 from match_game import MatchGame
 from data_cleanser import DataCleanser
-from selected_game import SelectedGame
 import math
 
 app = Flask(__name__)
 
 match_game = MatchGame()
 data_cleanser = DataCleanser()
-selected_game = SelectedGame()
 
 @app.route("/")
 def home():
@@ -19,12 +17,10 @@ def search():
     console = request.form["console"]
     game = request.form["game"]
     game = data_cleanser.remove_punctuation(game)
-    selected_game.game = game
-    selected_game.console = console
-    matched_titles = match_game.match_game(selected_game.console, selected_game.game)
+    matched_titles = match_game.match_game(console, game)
     if len(matched_titles) > 0:
         if len(matched_titles) == 1:
-            rows = match_game.select_from_db(selected_game.console,matched_titles[0])
+            rows = match_game.select_from_db(console,matched_titles[0])
             rows.reverse()
             if len(rows) >= 15:
                 mean_average = round(sum([row[5] for row in rows[:15]])/15,2)
@@ -36,18 +32,17 @@ def search():
                 median_average = round(sorted_prices[math.floor(len(rows) / 2)])
             else:
                 return render_template("error.html")
-            return render_template("results.html", game=selected_game.game, console=selected_game.console, rows=rows, len_rows=len(rows), mean_average=mean_average, median_average=median_average)
+            return render_template("results.html", game=game, console=console, rows=rows, len_rows=len(rows), mean_average=mean_average, median_average=median_average)
         else:
-            return render_template("multiple.html", matches=matched_titles)
+            return render_template("multiple.html", matches=matched_titles, console=console)
     else:
         return render_template("error.html")
 
-@app.route("/select", methods=["POST"])
+@app.route("/select", methods=["GET", "POST"])
 def select():
     game = request.form["game"]
-    print(game)
-    selected_game.game = game
-    rows = match_game.select_from_db(selected_game.console, selected_game.game)
+    console = request.form["console"]
+    rows = match_game.select_from_db(console, game)
     rows.reverse()
     if len(rows) > 0:
         if len(rows) >= 15:
@@ -58,7 +53,7 @@ def select():
             mean_average = round(sum([row[5] for row in rows]) / len(rows), 2)
             sorted_prices = sorted([row[5] for row in rows])
             median_average = sorted_prices[math.floor(len(rows) / 2)]
-        return render_template("results.html", game=selected_game.game, console=selected_game.console, rows=rows, len_rows=len(rows), mean_average=mean_average, median_average=median_average)
+        return render_template("results.html", game=game, console=console, rows=rows, len_rows=len(rows), mean_average=mean_average, median_average=median_average)
     else:
         return render_template("error.html")
 
